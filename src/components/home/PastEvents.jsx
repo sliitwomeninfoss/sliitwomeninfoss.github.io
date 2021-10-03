1a724bd816c9831899271882e8222ab37adb7828
chaitanyks <defcdr@gmail.com>
2021-10-04 00:43:16 +0530

    Code refector class to function. (#11)

--- src/components/home/PastEvents.jsx
+++ src/components/home/PastEvents.jsx
@@ -1,119 +1,117 @@
-import React, { Component } from 'react'
 import app1 from '../../assets/img/portfolio/app1.jpg'
 
-export default class PastEvents extends Component {
-    render() {
-        return (
-            <div>
-                <section id="past-events">
-                    <div class="container" data-aos="fade-up">
-                        <header class="section-header">
-                            <h3>Past Events</h3>
-                            <p>
-                                Laudem latine persequeris id sed, ex fabulas
-                                delectus quo. No vel partiendo abhorreant
-                                vituperatoribus.
-                            </p>
-                        </header>
+export default function PastEvents() {
 
-                        <div class="row row-eq-height justify-content-center">
-                            <div class="col-lg-4 mb-4">
-                                <div
-                                    class="card"
-                                    data-aos="zoom-in"
-                                    data-aos-delay="100"
-                                >
-                                    <img class="fa fa-diamond" src={app1} alt="blog"/>
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            Past Event Title
-                                        </h5>
-                                        <p class="card-text">
-                                            Event description goes here.
-                                            Voluptates nihil et quis omnis et
-                                            eaque omnis sint aut. Ducimus
-                                            dolorum aspernatur.
-                                        </p>
-                                        <a href="!#" class="readmore">
-                                            Read more{' '}
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
+    return (
+        <div>
+            <section id="past-events">
+                <div class="container" data-aos="fade-up">
+                    <header class="section-header">
+                        <h3>Past Events</h3>
+                        <p>
+                            Laudem latine persequeris id sed, ex fabulas
+                            delectus quo. No vel partiendo abhorreant
+                            vituperatoribus.
+                        </p>
+                    </header>
 
-                            <div class="col-lg-4 mb-4">
-                                <div
-                                    class="card"
-                                    data-aos="zoom-in"
-                                    data-aos-delay="200"
-                                >
-                                    <i class="fa fa-language"></i>
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            Voluptates dolores
-                                        </h5>
-                                        <p class="card-text">
-                                            Voluptates nihil et quis omnis et
-                                            eaque omnis sint aut. Ducimus
-                                            dolorum aspernatur.
-                                        </p>
-                                        <a href="!#" class="readmore">
-                                            Read more{' '}
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="col-lg-4 mb-4">
-                                <div
-                                    class="card"
-                                    data-aos="zoom-in"
-                                    data-aos-delay="300"
-                                >
-                                    <i class="fa fa-object-group"></i>
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            Eum ut aspernatur
-                                        </h5>
-                                        <p class="card-text">
-                                            Autem quod nesciunt eos ea aut amet
-                                            laboriosam ab. Eos quis porro in non
-                                            nemo ex.{' '}
-                                        </p>
-                                        <a href="!#" class="readmore">
-                                            Read more{' '}
-                                        </a>
-                                    </div>
+                    <div class="row row-eq-height justify-content-center">
+                        <div class="col-lg-4 mb-4">
+                            <div
+                                class="card"
+                                data-aos="zoom-in"
+                                data-aos-delay="100"
+                            >
+                                <img class="fa fa-diamond" src={app1} alt="blog" />
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Past Event Title
+                                    </h5>
+                                    <p class="card-text">
+                                        Event description goes here.
+                                        Voluptates nihil et quis omnis et
+                                        eaque omnis sint aut. Ducimus
+                                        dolorum aspernatur.
+                                    </p>
+                                    <a href="!#" class="readmore">
+                                        Read more{' '}
+                                    </a>
                                 </div>
                             </div>
                         </div>
 
-                        {/* Counter */}
-                        {/* <div class="row counters" data-aos="fade-up" data-aos-delay="100"> */}
-                        {/* Clients Counter */}
-                        {/* <div class="col-lg-3 col-6 text-center">
+                        <div class="col-lg-4 mb-4">
+                            <div
+                                class="card"
+                                data-aos="zoom-in"
+                                data-aos-delay="200"
+                            >
+                                <i class="fa fa-language"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Voluptates dolores
+                                    </h5>
+                                    <p class="card-text">
+                                        Voluptates nihil et quis omnis et
+                                        eaque omnis sint aut. Ducimus
+                                        dolorum aspernatur.
+                                    </p>
+                                    <a href="!#" class="readmore">
+                                        Read more{' '}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 mb-4">
+                            <div
+                                class="card"
+                                data-aos="zoom-in"
+                                data-aos-delay="300"
+                            >
+                                <i class="fa fa-object-group"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        Eum ut aspernatur
+                                    </h5>
+                                    <p class="card-text">
+                                        Autem quod nesciunt eos ea aut amet
+                                        laboriosam ab. Eos quis porro in non
+                                        nemo ex.{' '}
+                                    </p>
+                                    <a href="!#" class="readmore">
+                                        Read more{' '}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Counter */}
+                    {/* <div class="row counters" data-aos="fade-up" data-aos-delay="100"> */}
+                    {/* Clients Counter */}
+                    {/* <div class="col-lg-3 col-6 text-center">
                                 <span data-toggle="counter-up">274</span>
                                 <p>Clients</p>
                             </div> */}
-                        {/* Projects */}
-                        {/* <div class="col-lg-3 col-6 text-center">
+                    {/* Projects */}
+                    {/* <div class="col-lg-3 col-6 text-center">
                                 <span data-toggle="counter-up">421</span>
                                 <p>Projects</p>
                             </div> */}
-                        {/* Hours Of Support */}
-                        {/* <div class="col-lg-3 col-6 text-center">
+                    {/* Hours Of Support */}
+                    {/* <div class="col-lg-3 col-6 text-center">
                                 <span data-toggle="counter-up">1,364</span>
                                 <p>Hours Of Support</p>
                             </div> */}
-                        {/* Hard Workers */}
-                        {/* <div class="col-lg-3 col-6 text-center">
+                    {/* Hard Workers */}
+                    {/* <div class="col-lg-3 col-6 text-center">
                                 <span data-toggle="counter-up">18</span>
                                 <p>Hard Workers</p>
                             </div>
                         </div> */}
-                    </div>
-                </section>
-            </div>
-        )
-    }
+                </div>
+            </section>
+        </div>
+    )
 }
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,4 +1,3 @@
-import React, { Component } from 'react'
 import image from '../../assets/img/other/intro-img.svg'
 import AboutUs from './AboutUs'
 import Blogs from './Blogs'
@@ -8,52 +7,51 @@ import ContactUs from './ContactUs'
 import Mascot from './Mascot'
 import Hackathon from './Hackathon'
 
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <section id="intro" class="clearfix">
-                    <div class="container" data-aos="fade-up">
-                        <div
-                            class="intro-img"
-                            data-aos="zoom-out"
-                            data-aos-delay="200"
-                        >
-                            <img src={image} alt="" class="img-fluid" />
-                        </div>
-                        <div
-                            class="intro-info"
-                            data-aos="zoom-in"
-                            data-aos-delay="100"
-                        >
-                            <h2>
-                                SLIIT
-                                <br />
-                                <span>Women In FOSS</span>
-                                <br />
-                                Community!
-                            </h2>
-                            <div>
-                                <a
-                                    href="!#"
-                                    class="btn-get-started scrollto"
-                                >
-                                    Become a Member
-                                </a>
-                            </div>
+export default function Home() {
+
+    return (
+        <div>
+            <section id="intro" class="clearfix">
+                <div class="container" data-aos="fade-up">
+                    <div
+                        class="intro-img"
+                        data-aos="zoom-out"
+                        data-aos-delay="200"
+                    >
+                        <img src={image} alt="" class="img-fluid" />
+                    </div>
+                    <div
+                        class="intro-info"
+                        data-aos="zoom-in"
+                        data-aos-delay="100"
+                    >
+                        <h2>
+                            SLIIT
+                            <br />
+                            <span>Women In FOSS</span>
+                            <br />
+                            Community!
+                        </h2>
+                        <div>
+                            <a
+                                href="!#"
+                                class="btn-get-started scrollto"
+                            >
+                                Become a Member
+                            </a>
                         </div>
                     </div>
-                </section>
-                <main id="main">
-                    <AboutUs />
-                    <Mascot />
-                    <Hackathon />
-                    <PastEvents />
-                    <Blogs />
-                    <Team />
-                    <ContactUs />
-                </main>
-            </div>
-        )
-    }
+                </div>
+            </section>
+            <main id="main">
+                <AboutUs />
+                <Mascot />
+                <Hackathon />
+                <PastEvents />
+                <Blogs />
+                <Team />
+                <ContactUs />
+            </main>
+        </div>
+    )
 }

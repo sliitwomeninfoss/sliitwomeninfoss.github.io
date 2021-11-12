4860f5badb7b0b5b294552af7919b95f3726d51b
Modeesha-Kalani <modeeshakalani97@gmail.com>
2021-11-12 21:00:34 +0530

    footer modified

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,57 +1,21 @@
+import logo from '../../assets/img/logos/footer-logo.png';
+import 'font-awesome/css/font-awesome.min.css';
+import background from '../../assets/img/other/footer-topography.svg'
+
+
 export default function Footer() {
     return (
         <div>
             <footer id="footer">
-                <div class="footer-top">
+                <div class="footer-top" style={{ backgroundImage: 'url($background})' }}>
                     <div class="container">
                         <div class="row">
+
+
                             <div class="col-lg-4 col-md-6 footer-info">
-                                <h3>NewBiz</h3>
+                                <img src={logo} alt="logo" height="80" width="130" class="img-fluid just"/>
                                 <p>
-                                    Cras fermentum odio eu feugiat lide par
-                                    naso tierra.Justo eget nada terra videa
-                                    magna derita valies darta donna mare
-                                    fermentum iaculis eu non diam
-                                    phasellus.Scelerisque felis imperdiet
-                                    proin fermentum leo.Amet volutpat
-                                    consequat mauris nunc congue.
-                                </p>
-                            </div>
-
-                            <div class="col-lg-2 col-md-6 footer-links">
-                                <h4>Useful Links</h4>
-                                <ul>
-                                    <li>
-                                        <a href="!#">Home</a>
-                                    </li>
-                                    <li>
-                                        <a href="!#">About us</a>
-                                    </li>
-                                    <li>
-                                        <a href="!#">Services</a>
-                                    </li>
-                                    <li>
-                                        <a href="!#">Terms of service</a>
-                                    </li>
-                                    <li>
-                                        <a href="!#">Privacy policy</a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            <div class="col-lg-3 col-md-6 footer-contact">
-                                <h4>Contact Us</h4>
-                                <p>
-                                    A108 Adam Street <br />
-                                    New York, NY 535022
-                                    <br />
-                                    United States <br />
-                                    <strong>Phone: </strong> +1 5589 55488
-                                    55
-                                    <br />
-                                    <strong>Email: </strong> info
-                                    @example.com
-                                    <br />
+                                Welcome to SLIIT FOSS women community. We're a group of volunteers who believe in the usage of Free/Open Source Software (FOSS)
                                 </p>
 
                                 <div class="social-links">
@@ -71,22 +35,43 @@ export default function Footer() {
                                         <i class="fa fa-linkedin"></i>
                                     </a>
                                 </div>
+
                             </div>
 
-                            <div class="col-lg-3 col-md-6 footer-newsletter">
+
+                            <div class="col-lg-4 col-md-6 footer-links">
+                                <h4>Useful Links</h4>
+                              
+                                <ul>
+                                    <li>
+                                        <a href="!#">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Contact Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Blog</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Events</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Code of Conduct</a>
+                                    </li>
+                                </ul>
+                            </div>
+
+
+                            <div class="col-lg-4 col-md-6 footer-newsletter">
                                 <h4>Our Newsletter</h4>
                                 <p>
-                                    Tamen quem nulla quae legam multos aute
-                                    sint culpa legam noster magna veniam
-                                    enim veniam illum dolore legam minim
-                                    quorum culpa amet magna export quem
-                                    marada parida nodela caramase seza.
+                                    Don’t forget to subscribe to our news letters, enter your e-mail here.
                                 </p>
                                 <form
                                 // action=""
                                 // method="post"
                                 >
-                                    <input type="email" name="email" />
+                                    <input type="email" name="email"  placeholder="Email Address"/>
                                     <input
                                         type="submit"
                                         value="Subscribe"
@@ -98,15 +83,9 @@ export default function Footer() {
                 </div>
 
                 <div class="container">
-                    <div class="copyright">
-                        &copy; Copyright <strong>NewBiz</strong>
-                        .All Rights Reserved
-                    </div>
-                    <div class="credits">
-                        Designed by{' '}
-                        <a href="https://bootstrapmade.com/">
-                            BootstrapMade
-                        </a>
+                <div class="copyright">
+                    Copyright © 2021 | All Rights Reserved 
+                    <a href="sliitwif.org"> SLIIT WIF</a>
                     </div>
                 </div>
             </footer>

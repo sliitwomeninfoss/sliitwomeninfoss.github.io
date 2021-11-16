317b3dc67efe01063ca00c4ef3b3107de2c4a83f
Methmi Nugawela <methmi.nuga@gmail.com>
2021-11-16 19:28:38 +0530

    Created board member names constant file

---
+++ src/utilities/constants/BoardMembers.constants.jsx
@@ -0,0 +1,11 @@
+// Names of all board members displayed in the 'BOARD' page of website
+
+export const BOARD_MEMBER_NAMES ={
+    CLUB_LEAD: "Sewvandi",
+    MEMBERSHIP_AND_RECRUITING_LEAD: "Nishiki",
+    WEB_AND_GRAPHIC_TEAM_LEAD: "Uvini",
+    MARKETING_AND_PUBLICITY_LEAD: "Oshadi",
+    DEVELOPING_TEAM_LEAD: "Hansi",
+    CONTENT_WRITING_TEAM_LEAD: "Thathsarani",
+    TECHNICAL_LEAD: "Renu",
+}

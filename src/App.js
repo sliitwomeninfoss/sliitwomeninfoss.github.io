acdc7ca0d6390437a9b77829cca18132ae091dde
Modeesha-Kalani <modeeshakalani97@gmail.com>
2021-11-16 18:24:02 +0530

    new footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,13 @@ import CodeOfConduct from './components/home/CodeOfConduct'
 import Footer from './components/common/footer'
 import './App.css'
 
+// Font awesome icons
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
+
 function App() {
     return (
         <div className="App">
--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,4 +1,6 @@
 import logo from '../../assets/img/logos/footer-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import backgroundImage from '../../assets/img/other/footer-topography.svg'
 import 'font-awesome/css/font-awesome.min.css';
 
 
@@ -6,40 +8,43 @@ export default function Footer() {
     return (
         <div>
             <footer id="footer">
-                <div class="footer-top">
+                <div class="footer-top" style ={ { backgroundImage: `url('${backgroundImage}')`}}>
                     <div class="container">
                         <div class="row">
 
-
-                            <div class="col-lg-4 col-md-6 footer-info">
-                                <img src={logo} alt="logo" height="80" width="130" class="img-fluid just"/>
+                            {/* Logo and description */}
+                            <div class="col-lg-4 col-md-6 text-left footer-info">
+                                <img src={logo} alt="logo" height="80" width="130" class="img-fluid just footer-logo"/>
                                 <p>
                                 Welcome to SLIIT FOSS women community. We're a group of volunteers who believe in the usage of Free/Open Source Software (FOSS)
                                 </p>
 
+                                {/* Social media links */}
+                                <span class="connect">Connect with us</span>
+
                                 <div class="social-links">
-                                    <a href="!#" class="twitter">
-                                        <i class="fa fa-twitter"></i>
-                                    </a>
                                     <a href="!#" class="facebook">
-                                        <i class="fa fa-facebook"></i>
+                                    <FontAwesomeIcon icon={['fab', 'facebook']} />
                                     </a>
                                     <a href="!#" class="instagram">
-                                        <i class="fa fa-instagram"></i>
+                                    <FontAwesomeIcon icon={['fab', 'instagram']} />
+                                    </a>
+                                    <a href="!#" class="twitter">
+                                    <FontAwesomeIcon icon={['fab', 'twitter']} />
                                     </a>
                                     <a href="!#" class="linkedin">
-                                        <i class="fa fa-linkedin"></i>
+                                    <FontAwesomeIcon icon={['fab', 'linkedin']} />
                                     </a>
                                     <a href="!#" class="youtube">
-                                        <i class="fa fa-youtube"></i>
+                                    <FontAwesomeIcon icon={['fab', 'youtube']} />
                                     </a>
                                 </div>
 
                             </div>
 
-
-                            <div class="col-lg-4 col-md-6 footer-links">
-                                <h4>Useful Links</h4>
+                            {/* Useful Links */}
+                            <div class="col-lg-4 col-md-6 text-left footer-links">
+                                <h4 class="fheader">Useful Links</h4>
                               
                                 <ul>
                                     <li>
@@ -57,37 +62,71 @@ export default function Footer() {
                                     <li>
                                         <a href="!#">Code of Conduct</a>
                                     </li>
+                                    <li>
+                                        <a href="!#">GutHub</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Leaderboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Brand Assets</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Contribution Guidelines</a>
+                                    </li>
+                                    <li>
+                                        <a href="!#">Annual Report 2020</a>
+                                    </li>
                                 </ul>
                             </div>
 
-
-                            <div class="col-lg-4 col-md-6 footer-newsletter">
-                                <h4>Our Newsletter</h4>
-                                <p>
+                            {/* Subscribe */}
+                            <div class="col-lg-4 col-md-6  text-left footer-newsletter">
+                                <h4 class="fheader">Subscribe</h4>
+                                <p class="subscribe_text">
                                     Don’t forget to subscribe to our news letters, enter your e-mail here.
                                 </p>
-                                <form
+                                {/* Email form */}
+                                <div class="subscribe-form">
+                                <form 
                                 // action=""
                                 // method="post"
                                 >
                                     <input type="email" name="email"  placeholder="Email Address"/>
-                                    <input
-                                        type="submit"
-                                        value="Subscribe"
-                                    />
+                                    <button><i class="fa fa-envelope-o"></i></button>
                                 </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
+            {/* Copyright area */}
+            <div class="copyright-area">
                 <div class="container">
-                <div class="copyright">
-                    Copyright © 2021 | All Rights Reserved 
-                    <a href="sliitwif.org"> SLIIT WIF</a>
+                    <div class="row">
+                        <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                            <div class="copyright-text">
+                                <p>
+                                    Copyright &copy; 2021 | All Rights Reserved 
+                                    <a href="https://sliitwif.org"> SLIIT WIF</a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                            <div class="footer-menu">
+                                <ul>
+                                    <li><a href="/">Home</a></li>
+                                    <li><a href="#">Terms</a></li>
+                                    <li><a href="#">Privacy</a></li>
+                                    <li><a href="#">Policy</a></li>
+                                    <li><a href="/contact">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </footer>
-        </div>
-    )
+            </div>
+    </footer>
+</div>)
 }

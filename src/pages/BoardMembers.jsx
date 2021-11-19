b9233f31f3899752ba9ed58fcca2ab95ba1901fb
Methmi Nugawela <methmi.nuga@gmail.com>
2021-11-19 17:14:45 +0530

    Updated board member card style

--- src/pages/BoardMembers.jsx
+++ src/pages/BoardMembers.jsx
@@ -75,7 +75,7 @@ export default function Team() {
                     <div class="section-header">
                         <h3>Board of Executives 20/21</h3>
                     </div>
-                    <div class="row" style={{justifyContent: "center"}}>
+                    <div class="row member-row">
                         <div
                             class="col-lg-3 col-md-6"
                             data-aos="zoom-out"
@@ -85,22 +85,41 @@ export default function Team() {
                                 <img src={Team2} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.MEMBERSHIP_AND_RECRUITING_LEAD.NAME}</h4>
-                                        <span>Membership & Recruiting Lead</span>
                                         <div class="social">
-                                            <a href={BOARD_MEMBER_DETAILS.MEMBERSHIP_AND_RECRUITING_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .MEMBERSHIP_AND_RECRUITING_LEAD
+                                                        .FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.MEMBERSHIP_AND_RECRUITING_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .MEMBERSHIP_AND_RECRUITING_LEAD
+                                                        .LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="member-text">
+                                <h4>
+                                    {
+                                        BOARD_MEMBER_DETAILS
+                                            .MEMBERSHIP_AND_RECRUITING_LEAD.NAME
+                                    }
+                                </h4>
+                                <span>Membership & Recruiting Lead</span>
+                            </div>
                         </div>
                         <div
-                            class="col-lg-4 col-md-6"
+                            class="col-lg-3 col-md-6"
                             data-aos="zoom-out"
                             data-aos-delay="100"
                         >
@@ -108,18 +127,30 @@ export default function Team() {
                                 <img src={Team4} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.CLUB_LEAD.NAME}</h4>
-                                        <span><h5>Club Lead</h5></span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.CLUB_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .CLUB_LEAD.FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.CLUB_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .CLUB_LEAD.LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>{BOARD_MEMBER_DETAILS.CLUB_LEAD.NAME}</h4>
+                                <span>Club Lead</span>
                             </div>
                         </div>
                         <div
@@ -131,22 +162,41 @@ export default function Team() {
                                 <img src={Team2} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.WEB_AND_GRAPHIC_TEAM_LEAD.NAME}</h4>
-                                        <span>Web & Graphic Team Lead</span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.WEB_AND_GRAPHIC_TEAM_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .WEB_AND_GRAPHIC_TEAM_LEAD
+                                                        .FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.WEB_AND_GRAPHIC_TEAM_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .WEB_AND_GRAPHIC_TEAM_LEAD
+                                                        .LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>
+                                    {
+                                        BOARD_MEMBER_DETAILS
+                                            .WEB_AND_GRAPHIC_TEAM_LEAD.NAME
+                                    }
+                                </h4>
+                                <span>Web & Graphic Team Lead</span>
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row member-row">
                         <div
                             class="col-lg-3 col-md-6"
                             data-aos="zoom-out"
@@ -156,18 +206,37 @@ export default function Team() {
                                 <img src={Team4} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.MARKETING_AND_PUBLICITY_LEAD.NAME}</h4>
-                                        <span>Marketing & Publicity Lead</span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.MARKETING_AND_PUBLICITY_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .MARKETING_AND_PUBLICITY_LEAD
+                                                        .FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.MARKETING_AND_PUBLICITY_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .MARKETING_AND_PUBLICITY_LEAD
+                                                        .LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>
+                                    {
+                                        BOARD_MEMBER_DETAILS
+                                            .MARKETING_AND_PUBLICITY_LEAD.NAME
+                                    }
+                                </h4>
+                                <span>Marketing & Publicity Lead</span>
                             </div>
                         </div>
                         <div
@@ -179,18 +248,37 @@ export default function Team() {
                                 <img src={Team2} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.DEVELOPING_TEAM_LEAD.NAME}</h4>
-                                        <span>Developing Team Lead</span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.DEVELOPING_TEAM_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .DEVELOPING_TEAM_LEAD
+                                                        .FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.DEVELOPING_TEAM_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .DEVELOPING_TEAM_LEAD
+                                                        .LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>
+                                    {
+                                        BOARD_MEMBER_DETAILS
+                                            .DEVELOPING_TEAM_LEAD.NAME
+                                    }
+                                </h4>
+                                <span>Developing Team Lead</span>
                             </div>
                         </div>
                         <div
@@ -202,18 +290,37 @@ export default function Team() {
                                 <img src={Team4} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.CONTENT_WRITING_TEAM_LEAD.NAME}</h4>
-                                        <span>Content Writing Team Lead</span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.CONTENT_WRITING_TEAM_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .CONTENT_WRITING_TEAM_LEAD
+                                                        .FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.CONTENT_WRITING_TEAM_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .CONTENT_WRITING_TEAM_LEAD
+                                                        .LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>
+                                    {
+                                        BOARD_MEMBER_DETAILS
+                                            .CONTENT_WRITING_TEAM_LEAD.NAME
+                                    }
+                                </h4>
+                                <span>Content Writing Team Lead</span>
                             </div>
                         </div>
                         <div
@@ -225,18 +332,32 @@ export default function Team() {
                                 <img src={Team2} class="img-fluid" alt="" />
                                 <div class="member-info">
                                     <div class="member-info-content">
-                                        <h4>{BOARD_MEMBER_DETAILS.TECHNICAL_LEAD.NAME}</h4>
-                                        <span>Technical Lead</span>
                                         <div class="social">
-                                        <a href={BOARD_MEMBER_DETAILS.TECHNICAL_LEAD.FACEBOOK}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .TECHNICAL_LEAD.FACEBOOK
+                                                }
+                                            >
                                                 <i class="fa fa-facebook"></i>
                                             </a>
-                                            <a href={BOARD_MEMBER_DETAILS.TECHNICAL_LEAD.LINKEDIN}>
+                                            <a
+                                                href={
+                                                    BOARD_MEMBER_DETAILS
+                                                        .TECHNICAL_LEAD.LINKEDIN
+                                                }
+                                            >
                                                 <i class="fa fa-linkedin"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="member-text">
+                                <h4>
+                                    {BOARD_MEMBER_DETAILS.TECHNICAL_LEAD.NAME}
+                                </h4>
+                                <span>Technical Lead</span>
                             </div>
                         </div>
                     </div>

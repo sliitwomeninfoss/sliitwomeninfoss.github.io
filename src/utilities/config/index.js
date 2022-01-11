a3731886d1c3f86ebc3077c1515fa66b3167be0c
Dulya Perera <dulyakemali@gmail.com>
2022-01-11 12:49:48 +0530

    Fixing compiler error

--- src/utilities/config/index.js
+++ src/utilities/config/index.js
@@ -1,3 +1,4 @@
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default {
     MEDIUM_BLOG_URL:
         'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/sliitwif',

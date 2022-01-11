e6e5bb25627501b16cef40e513b7af04d496a63a
Dulya Perera <57215584+Dulyaaa@users.noreply.github.com>
2022-01-11 12:53:46 +0530

    Linked with Medium Blog Posts (#96)

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,7 +1,25 @@
-import BlogsData from '../utilities/data/Blogs.json'
+import { useState, useEffect } from 'react'
+import Config from '../utilities/config'
 import logo from '../assets/img/logos/WIF-Dark.png'
 
 export default function Blogs() {
+
+    const [blog, setBlog] = useState({
+        item: [],
+        error: null
+    })
+
+    const axios = require('axios').default
+
+    useEffect(() => {
+        axios.get(Config.MEDIUM_BLOG_URL)
+            .then(info => {
+                const posts = info.data.items.filter(post => post.categories.length > 0);
+                setBlog({ item: posts })
+            })
+            .catch(err => setBlog({ error: err.message }))
+    }, [axios])
+
     return (
         <div>
             <header id="header" class="fixed-top">
@@ -89,26 +107,26 @@ export default function Blogs() {
 
 
                     <div class="row">
-                        {BlogsData.Blogs.map((blog, index) => (
+                        {blog.item.map((blog, index) => (
                             <div class="col-md-4">
                                 <div class="card-content">
                                     <div class="card-img">
-                                        <img src={blog.image} alt="" />
-                                        <span><h4>Published On: {blog.published_date}</h4></span>
+                                        <img src={blog.thumbnail} alt="" />
+                                        <span><h4>Published On: {blog.pubDate.split(' ', 1)}</h4></span>
                                     </div>
                                     <div class="card-desc">
-                                        <h3>{blog.blog_title}</h3>
+                                        <h3>{blog.title}</h3>
                                         <br />
                                         {/* <p>{blog.description}</p> */}
-                                        <p> {blog.tags.length > 0 ?
-                                            blog.tags.map((tag, index) => (
+                                        <p> {blog.categories.length > 0 ?
+                                            blog.categories.map((tag, index) => (
                                                 <span>{tag}</span>
                                             ))
                                             : <div />
                                         }
                                         </p>
                                     </div>
-                                        <a href={blog.blog_url} class="read-btn" target="_blank" rel="noreferrer">Read More</a>
+                                    <a href={blog.link} class="read-btn" target="_blank" rel="noreferrer">Read More</a>
                                 </div>
                             </div>
                         ))}

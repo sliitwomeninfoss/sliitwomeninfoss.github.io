8259c332b29dfecb7bfac995254c32c3104b6896
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-03-15 01:51:32 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
     <React.StrictMode>

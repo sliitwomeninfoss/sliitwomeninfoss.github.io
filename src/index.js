07e1266b28d77864ed2791ed5b4da231f002bb17
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-03-14 23:37:41 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom'
 
 ReactDOM.render(
     <React.StrictMode>
-        <HashRouter>
+        <HashRouter basename="/sliitwomeninfoss.github.io">
             <App />
         </HashRouter>
     </React.StrictMode>,

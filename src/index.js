24c5363f51a4fb37eb90fe9d0c1d5731a2dc23f9
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-03-16 23:08:42 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+
+const basename =
+    process.env.NODE_ENV === 'production' ? '/sliitwomeninfoss.github.io' : '/'
 
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter basename={basename}>
         <App />
-    </React.StrictMode>,
+    </BrowserRouter>,
     document.getElementById('root')
 )
 

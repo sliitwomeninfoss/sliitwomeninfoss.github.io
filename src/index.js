02222724977d53e75fc348f81b619eb3c2ba25b7
SandalikaAriyrathna <dilsandu96@gmail.com>
2023-03-18 23:59:13 +0530

    indexjs modified

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-    <React.StrictMode>
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
         <App />
-    </React.StrictMode>
+    </BrowserRouter>,
+    document.getElementById('root')
 )
